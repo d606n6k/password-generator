a3f04a63170e8665a40de1abf39dbb2f8a242bf4
William Lucht <aaronlucht@gmail.com>
2021-03-18 17:34:03 -0700

    added a user story diagram to project. Added password characters to variables inside our js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// lowercase characters
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// uppercase characters
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// special characters
+var specialChar = ["!","@","^","*","$","/","~","."];
+
+// variable console.logs
+console.log(lowerCase);
+console.log(upperCase);
+console.log(specialChar);
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
