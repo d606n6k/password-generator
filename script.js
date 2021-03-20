35d3cdfff9b518080985747afcc5b9406d68d61a
William Lucht <aaronlucht@gmail.com>
2021-03-19 17:08:39 -0700

    added if statements to the prompt windows for the generatePassword function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 // special characters
 var specialChar = ["!","@","^","*","$","/","~","."];
-
+var passChoices = [];
 // variable console.logs
 // console.log(lowerCase);
 // console.log(upperCase);
@@ -18,28 +18,42 @@ function generatePassword(){
     // IF the user enters something other than a number value ->
         // DO alert the user that "Please enter a number!"
         // 'STOP THE FUNCTION' aka 'return'
-    if (passwordLength < 8 || passwordLength > 128){
-        alert("Please enter a number greater than 8 and less than 128");
-        return;
-    }
-
-    var charTypes = window.confirm("Would you like to use Special Characters in your password?");
-    
-    var upperChars = window.confirm("Would you like to use Uppercase characters in your password?");
-    console.log(upperChars);
-    
-    var lowerChars = window.confirm("Would you like to use Lowercase characters in your password?");
-    console.log(lowerChars);
-
-
-    // IF the entered number is less than 8
+         // IF the entered number is less than 8
         // alert the user that we need a number greater than or equal to 8 "enter 8 or more"
         // 'STOP THE FUNCTION!' aka 'return'
     // IF the entered number is greater than 128
         // alert the user that we need a number less than or equal to 128
         // 'STOP THE THE FUNCTION!' aka 'return'
 
-    return "Password";
+    if (passwordLength < 8 || passwordLength > 128){
+        alert("Please enter a number greater than 8 and less than 128");
+        return;
+    }
+
+    var specialChars = window.confirm("Would you like to use Special Characters in your password?");
+
+    var upperChars = window.confirm("Would you like to use Uppercase characters in your password?");
+    // console.log(upperChars);
+    
+    var lowerChars = window.confirm("Would you like to use Lowercase characters in your password?");
+    // console.log(lowerChars);
+
+    if (specialChars === true){
+        // we want to add special characters into the passChoices array
+        passChoices.concat(specialChar);
+        passChoices.push(specialChar);
+        console.log(passChoices);
+    }
+    if (upperChars === true){
+        // we want to add upper case characters into the passChoices array
+        passChoices.push(upperChars);
+        console.log(passChoices);
+    }
+    if (lowerChars === true){
+        // we want to add lower case characters into the passChoices array
+        passChoices.push(lowerChars);
+        console.log(passChoices);   
+    }
 
 };
 
